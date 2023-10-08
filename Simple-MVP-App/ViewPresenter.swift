76dd537f672a4d3edbf569682e48d648145ee1e4
Efe Adzhar <Efe.Adzhar@nordstrom.com>
2023-10-08 18:44:20 +0200

    Additional commit

---
+++ Simple-MVP-App/ViewPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  ViewPresenter.swift
+//  Simple-MVP-App
+//
+//  Created by Efe on 06.10.2023.
+//
+
+import Foundation
+
+protocol ViewPresenterDelegate {
+    
+}
+class ViewPresenter {
+    
+}
--- Simple-MVP-App/presenter/ViewPresenter.swift
+++ Simple-MVP-App/presenter/ViewPresenter.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+protocol ViewPresenterDelegate : AnyObject {
+    func newData(_ newData: String)
+}
+
+class ViewPresenter {
+    private weak var delegate : ViewPresenterDelegate?
+    private let modelService = LabelModelService()
+    private var count : Int?
+    
+    init() {
+        modelService.setDelegate(delegate: self)
+    }
+    
+    public func setViewDelegate(delegate : ViewPresenterDelegate) {
+        self.delegate = delegate
+    }
+    
+    public func changeNumberInLabel(label : UILabel) {
+        modelService.changeData(label: label)
+        delegate?.newData(String(count!))
+    }
+}
+
+extension ViewPresenter : LabelModelDelegate {
+    func changeData(data: Int) {
+        self.count = data
+    }
+}
